b1c0372190965f0ae2ac598673d679064c2fe4d0
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-06-28 11:14:14 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,25 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let a = result.input;   
+    let count = 0;
+    let count2 = 0;
+    for (let i = 0; i < a.length; i++) {
+
+        if (a[i] == '2') {
+            count++;
+        }
+        if (a[i] == '5') {
+            count2++;
+        }
+       
+    }
+    if (count > count2) {
+        console.log('2');
+    }else if(count < count2) {
+        console.log('5');
+    }else {
+        console.log('=');
+    }
   
 });
