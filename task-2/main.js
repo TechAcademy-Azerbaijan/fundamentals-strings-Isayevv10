5f07240f216be3b9c1300bd18aae0a2c555a53dd
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-06-28 10:54:24 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -4,6 +4,18 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    let a = result.input.split(',')[0];
+    
+    for (let i = 0; i < a.length; i++) {
+        if (a[0] > a[a.length - 1]) {
+            console.log(a[0]);
+            break;
+        }else if(a[0] < a[a.length - 1]) {
+            console.log(a[a.length - 1]);
+            break;
+        }else {
+            console.log('=');
+            break;
+        }
+    }
 });
