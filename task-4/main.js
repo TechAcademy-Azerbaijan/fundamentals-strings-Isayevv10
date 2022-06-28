f45e9e816d4afb5acaa8c4076f92d1f5da67d676
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-06-28 12:16:33 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -4,6 +4,17 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let a = result.input;  
+    let count = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] == "!") {
+            count++;
+        }else if(a[i] == "?") {
+            count++;
+        }else if(a[i] == ".") {
+            count++;
+        }
+    }
+    console.log(count);
   
 });
