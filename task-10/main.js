58e79d58ca38671fd62c297cb5c1ce85e8497559
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-06-30 15:48:05 +0400

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -4,6 +4,15 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let str = result.input.split('');
+   
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != ' ') {
+            var firstIndex = str.indexOf(' ');
+            var lastIndex = str.lastIndexOf(' '); 
+        }
+    }
+    console.log(firstIndex);
+    console.log(lastIndex);
   
 });
