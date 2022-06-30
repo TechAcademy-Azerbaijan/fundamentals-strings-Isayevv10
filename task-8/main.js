eb055f8ccbcacb028cfd60b48638fe6d32fa5d95
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-06-30 12:23:02 +0400

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -4,6 +4,14 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let a = result.input;  
+    let str = a.split(" ");
+    let str1;
+    let str2 = [];
+    for (let i = 0; i < str.length; i++) {
+        let str1 = str[i].substring(0,1).toUpperCase() + str[i].slice(1);
+        str2.push(str1);
+    }
+    console.log(str2.join(" "));
   
 });
