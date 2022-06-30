b5c71d9e338bf56686a67ad52580c37a8e6241d4
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-06-30 13:04:11 +0400

    Update main.js

--- task-9/main.js
+++ task-9/main.js
@@ -1,5 +1,13 @@
 
-
+  let s = "This is a cat sitting on a table";
+  let c = "t";
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+      if (s[i].toLocaleLowerCase() == c) {
+          count++;
+      }        
+  }
+  console.log(count);
 
 
 
