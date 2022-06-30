d86c79d13168b4e506654dc52837c90c2e0e3330
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-06-30 13:01:45 +0400

    Update main.js

--- task-9/main.js
+++ task-9/main.js
@@ -1,17 +1,24 @@
-const prompt = require('prompt');
-prompt.start();
 
-prompt.get('input',function(err, result) { 
-    let s = result.input.split(',')[0].toLocaleLowerCase(); 
-    let c = result.input.split(',')[1];
-    let count = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] == c) {
-            count++;
-        }        
-    }
-    console.log(count);
-});
+
+
+
+
+
+
+// const prompt = require('prompt');
+// prompt.start();
+
+// prompt.get('input',function(err, result) { 
+//     let s = result.input.split(',')[0].toLocaleLowerCase(); 
+//     let c = result.input.split(',')[1];
+//     let count = 0;
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] == c) {
+//             count++;
+//         }        
+//     }
+//     console.log(count);
+// });
 
 
 
