eb3221dde6f56fd3ee95226925b4d681cd81446f
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-06-30 13:09:48 +0400

    Update main.js

--- task-9/main.js
+++ task-9/main.js
@@ -1,32 +1,32 @@
 
-//   let s = "This is a cat sitting on a table";
-//   let c = "t";
-//   let count = 0;
-//   for (let i = 0; i < s.length; i++) {
-//       if (s[i].toLocaleLowerCase() == c) {
-//           count++;
-//       }        
-//   }
-//   console.log(count);
+  let s = "This is a cat sitting on a table";
+  let c = "t";
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+      if (s[i].toLocaleLowerCase() == c) {
+          count++;
+      }        
+  }
+  console.log(count);
 
 
 
 
 
-const prompt = require('prompt');
-prompt.start();
+// const prompt = require('prompt');
+// prompt.start();
 
-prompt.get('input',function(err, result) { 
-    let s = result.input.split(',')[0].toLocaleLowerCase(); 
-    let c = result.input.split(',')[1];
-    let count = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] == c) {
-            count++;
-        }        
-    }
-    console.log(count);
-});
+// prompt.get('input',function(err, result) { 
+//     let s = result.input.split(',')[0].toLocaleLowerCase(); 
+//     let c = result.input.split(',')[1];
+//     let count = 0;
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] == c) {
+//             count++;
+//         }        
+//     }
+//     console.log(count);
+// });
 
 
 
